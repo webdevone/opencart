3a0be58fcb622ae0a8b0ef504c5b28eada352fe1
Desarrollo <desarrollo@webifica.com>
2023-06-14 17:13:43 -0500

    quitar comentario

--- upload/catalog/controller/pezglobo/services/PezGloboProductIntermediaService.php
+++ upload/catalog/controller/pezglobo/services/PezGloboProductIntermediaService.php
@@ -1,23 +1,20 @@
 <?php
 
-final class PezGloboProductIntermediaService 
+final class PezGloboProductIntermediaService
 {
     public static function save($model, array $data)
     {
-        $products = $model->exists([
-            'model' => htmlspecialchars_decode($data['model']),
-            'manufacturer_id' => $data['manufacturer_id']
-        ]);
+        $products = $model->exists(
+            [
+                'model' => htmlspecialchars_decode($data['model']),
+                'manufacturer_id' => $data['manufacturer_id']
+            ]
+        );
 
         if (!$products) {
             $model->addProduct($data);
             return;
         }
-        
-        echo "---------------------------------------------------------- <br>";
-        echo "encontrado " . $data['model'] . " ? : <br>";
-        var_dump($products);
-        echo "<br>";
         
         $model->editProduct($products, $data);
     }
