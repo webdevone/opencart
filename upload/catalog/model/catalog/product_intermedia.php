ec60cb3217720638104be4f17109940bef7c68b7
Desarrollo <desarrollo@webifica.com>
2023-06-14 17:15:52 -0500

    cambio de nombre de modelo product intermedia

---
+++ upload/catalog/model/catalog/product_intermedia.php
@@ -0,0 +1,287 @@
+<?php
+class ModelCatalogProductIntermedia extends Model {
+
+	public function getProduct($product_id) {
+		$query = $this->db->query("SELECT DISTINCT *, pd.name AS name, p.image, m.name AS manufacturer, (SELECT points FROM " . DB_PREFIX . "product_reward pr WHERE pr.product_id = p.product_id AND pr.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "') AS reward, (SELECT ss.name FROM " . DB_PREFIX . "stock_status ss WHERE ss.stock_status_id = p.stock_status_id AND ss.language_id = '" . (int)$this->config->get('config_language_id') . "') AS stock_status, (SELECT wcd.unit FROM " . DB_PREFIX . "weight_class_description wcd WHERE p.weight_class_id = wcd.weight_class_id AND wcd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS weight_class, (SELECT lcd.unit FROM " . DB_PREFIX . "length_class_description lcd WHERE p.length_class_id = lcd.length_class_id AND lcd.language_id = '" . (int)$this->config->get('config_language_id') . "') AS length_class, (SELECT AVG(rating) AS total FROM " . DB_PREFIX . "review r1 WHERE r1.product_id = p.product_id AND r1.status = '1' GROUP BY r1.product_id) AS rating, (SELECT COUNT(*) AS total FROM " . DB_PREFIX . "review r2 WHERE r2.product_id = p.product_id AND r2.status = '1' GROUP BY r2.product_id) AS reviews, p.sort_order FROM " . DB_PREFIX . "product_intermedia p LEFT JOIN " . DB_PREFIX . "product_description_intermedia pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store_intermedia p2s ON (p.product_id = p2s.product_id) LEFT JOIN " . DB_PREFIX . "manufacturer m ON (p.manufacturer_id = m.manufacturer_id) WHERE p.product_id = '" . (int)$product_id . "' AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "'");
+
+		if ($query->num_rows) {
+			return array(
+				'product_id'       => $query->row['product_id'],
+				'name'             => $query->row['name'],
+				'description'      => $query->row['description'],
+				'meta_title'       => $query->row['meta_title'],
+				'meta_description' => $query->row['meta_description'],
+				'meta_keyword'     => $query->row['meta_keyword'],
+				'tag'              => $query->row['tag'],
+				'model'            => $query->row['model'],
+				'sku'              => $query->row['sku'],
+				'upc'              => $query->row['upc'],
+				'ean'              => $query->row['ean'],
+				'jan'              => $query->row['jan'],
+				'isbn'             => $query->row['isbn'],
+				'mpn'              => $query->row['mpn'],
+				'location'         => $query->row['location'],
+				'quantity'         => $query->row['quantity'],
+				'stock_status'     => $query->row['stock_status'],
+				'image'            => $query->row['image'],
+				'manufacturer_id'  => $query->row['manufacturer_id'],
+				'manufacturer'     => $query->row['manufacturer'],
+				'price'            => ($query->row['discount'] ? $query->row['discount'] : $query->row['price']),
+				'special'          => $query->row['special'],
+				'reward'           => $query->row['reward'],
+				'points'           => $query->row['points'],
+				'tax_class_id'     => $query->row['tax_class_id'],
+				'date_available'   => $query->row['date_available'],
+				'weight'           => $query->row['weight'],
+				'weight_class_id'  => $query->row['weight_class_id'],
+				'length'           => $query->row['length'],
+				'width'            => $query->row['width'],
+				'height'           => $query->row['height'],
+				'length_class_id'  => $query->row['length_class_id'],
+				'subtract'         => $query->row['subtract'],
+				'rating'           => round(($query->row['rating']===null) ? 0 : $query->row['rating']),
+				'reviews'          => $query->row['reviews'] ? $query->row['reviews'] : 0,
+				'minimum'          => $query->row['minimum'],
+				'sort_order'       => $query->row['sort_order'],
+				'status'           => $query->row['status'],
+				'date_added'       => $query->row['date_added'],
+				'date_modified'    => $query->row['date_modified'],
+				'viewed'           => $query->row['viewed']
+			);
+		} else {
+			return false;
+		}
+	}
+
+	public function getProducts($data = array()) {
+		$sql = "SELECT p.product_id";
+		
+		if (!empty($data['filter_category_id'])) {
+		} else {
+			$sql .= " FROM " . DB_PREFIX . "product p";
+		}
+
+		$sql .= " LEFT JOIN " . DB_PREFIX . "product_description_intermedia pd ON (p.product_id = pd.product_id)";
+
+		if (!empty($data['filter_name']) || !empty($data['filter_tag'])) {
+			$sql .= " AND (";
+
+			if (!empty($data['filter_name'])) {
+				$implode = array();
+
+				$words = explode(' ', trim(preg_replace('/\s+/', ' ', $data['filter_name'])));
+
+				foreach ($words as $word) {
+					$implode[] = "pd.name LIKE '%" . $this->db->escape($word) . "%'";
+				}
+
+				if ($implode) {
+					$sql .= " " . implode(" AND ", $implode) . "";
+				}
+
+				if (!empty($data['filter_description'])) {
+					$sql .= " OR pd.description LIKE '%" . $this->db->escape($data['filter_name']) . "%'";
+				}
+			}
+
+			if (!empty($data['filter_name']) && !empty($data['filter_tag'])) {
+				$sql .= " OR ";
+			}
+
+			if (!empty($data['filter_tag'])) {
+				$implode = array();
+
+				$words = explode(' ', trim(preg_replace('/\s+/', ' ', $data['filter_tag'])));
+
+				foreach ($words as $word) {
+					$implode[] = "pd.tag LIKE '%" . $this->db->escape($word) . "%'";
+				}
+
+				if ($implode) {
+					$sql .= " " . implode(" AND ", $implode) . "";
+				}
+			}
+
+			if (!empty($data['filter_name'])) {
+				$sql .= " OR LCASE(p.model) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.sku) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.upc) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.ean) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.jan) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.isbn) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.mpn) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+			}
+
+			$sql .= ")";
+		}
+
+		if (!empty($data['filter_manufacturer_id'])) {
+			$sql .= " AND p.manufacturer_id = '" . (int)$data['filter_manufacturer_id'] . "'";
+		}
+
+		$sql .= " GROUP BY p.product_id";
+
+		$sort_data = array(
+			'pd.name',
+			'p.model',
+			'p.quantity',
+			'p.price',
+			'rating',
+			'p.sort_order',
+			'p.date_added'
+		);
+
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
+				$sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
+			} elseif ($data['sort'] == 'p.price') {
+				$sql .= " ORDER BY (CASE WHEN special IS NOT NULL THEN special WHEN discount IS NOT NULL THEN discount ELSE p.price END)";
+			} else {
+				$sql .= " ORDER BY " . $data['sort'];
+			}
+		} else {
+			$sql .= " ORDER BY p.sort_order";
+		}
+
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
+			$sql .= " DESC, LCASE(pd.name) DESC";
+		} else {
+			$sql .= " ASC, LCASE(pd.name) ASC";
+		}
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$product_data = array();
+		echo $sql;
+		$query = $this->db->query($sql);
+
+		foreach ($query->rows as $result) {
+			$product_data[$result['product_id']] = $this->getProduct($result['product_id']);
+		}
+
+		return $product_data;
+	}
+
+	public function getTotalProducts($data = array()) {
+		$sql = "SELECT COUNT(DISTINCT p.product_id) AS total";
+
+		$sql .= " LEFT JOIN " . DB_PREFIX . "product_description_intermedia pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store_intermedia p2s ON (p.product_id = p2s.product_id) WHERE pd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p.status = '1' AND p.date_available <= NOW() AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "'";
+
+		if (!empty($data['filter_name']) || !empty($data['filter_tag'])) {
+			$sql .= " AND (";
+
+			if (!empty($data['filter_name'])) {
+				$implode = array();
+
+				$words = explode(' ', trim(preg_replace('/\s+/', ' ', $data['filter_name'])));
+
+				foreach ($words as $word) {
+					$implode[] = "pd.name LIKE '%" . $this->db->escape($word) . "%'";
+				}
+
+				if ($implode) {
+					$sql .= " " . implode(" AND ", $implode) . "";
+				}
+
+				if (!empty($data['filter_description'])) {
+					$sql .= " OR pd.description LIKE '%" . $this->db->escape($data['filter_name']) . "%'";
+				}
+			}
+
+			if (!empty($data['filter_name']) && !empty($data['filter_tag'])) {
+				$sql .= " OR ";
+			}
+
+			if (!empty($data['filter_tag'])) {
+				$implode = array();
+
+				$words = explode(' ', trim(preg_replace('/\s+/', ' ', $data['filter_tag'])));
+
+				foreach ($words as $word) {
+					$implode[] = "pd.tag LIKE '%" . $this->db->escape($word) . "%'";
+				}
+
+				if ($implode) {
+					$sql .= " " . implode(" AND ", $implode) . "";
+				}
+			}
+
+			if (!empty($data['filter_name'])) {
+				$sql .= " OR LCASE(p.model) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.sku) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.upc) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.ean) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.jan) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.isbn) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.mpn) = '" . $this->db->escape(utf8_strtolower($data['filter_name'])) . "'";
+			}
+
+			$sql .= ")";
+		}
+
+		if (!empty($data['filter_manufacturer_id'])) {
+			$sql .= " AND p.manufacturer_id = '" . (int)$data['filter_manufacturer_id'] . "'";
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->row['total'];
+	}
+
+	// Pez Globo
+	public function addProduct($data) {
+		$this->db->query("INSERT INTO " . DB_PREFIX . "product_intermedia SET model = '" . $this->db->escape($data['model']) . "', quantity = '" . (int)$data['quantity'] . "', availability = '" . $data['availability'] . "', manufacturer_id = '" . (int)$data['manufacturer_id'] . "', price = '" . (float)$data['price'] . "', weight = '" . (float)$data['weight'] . "', store_id = '" . $data['store_id'] . "', link = '" . 
+		$data['link'] . "', date_added = NOW(), date_modified = NOW()");
+
+		$product_id = $this->db->getLastId();
+
+		if (isset($data['image'])) {
+			$this->db->query("UPDATE " . DB_PREFIX . "product_intermedia SET image = '" . $this->db->escape($data['image']) . "' WHERE product_id = '" . (int)$product_id . "'");
+		}
+
+		foreach ($data['product_description'] as $language_id => $value) {
+			$this->db->query("INSERT INTO " . DB_PREFIX . "product_description_intermedia SET product_id = '" . (int)$product_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "', tag = '" . $this->db->escape($value['tag']) . "'");
+		}
+
+		$this->cache->delete('product');
+
+		return $product_id;
+	}
+
+	public function exists($data) {
+		$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product_intermedia p WHERE p.model = '" . $data['model'] . "' AND p.manufacturer_id = '" . (int)$data['manufacturer_id'] . "'");
+		
+		if (empty($query->num_rows)) {
+			return false;
+		}
+
+		return $query->row['product_id'];
+	}
+	public function editProduct($product_id, $data) {
+		$this->db->query("UPDATE " . DB_PREFIX . "product_intermedia SET model = '" . $this->db->escape($data['model']) . "', quantity = '" . (int)$data['quantity'] . "', availability = '" . $data['availability'] . "', manufacturer_id = '" . (int)$data['manufacturer_id'] . "', price = '" . (float)$data['price'] . "', weight = '" . (float)$data['weight'] . "', store_id = '" . $data['store_id'] . "', link = '" . 
+		$data['link'] . "', date_modified = NOW() WHERE product_id = '" . (int)$product_id . "'");
+		
+		if (isset($data['image'])) {
+			$this->db->query("UPDATE " . DB_PREFIX . "product_intermedia SET image = '" . $this->db->escape($data['image']) . "' WHERE product_id = '" . (int)$product_id . "'");
+		}
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "product_description_intermedia WHERE product_id = '" . (int)$product_id . "'");
+
+		foreach ($data['product_description'] as $language_id => $value) {
+			$this->db->query("INSERT INTO " . DB_PREFIX . "product_description_intermedia SET product_id = '" . (int)$product_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "', description = '" . $this->db->escape($value['description']) . "', tag = '" . $this->db->escape($value['tag']) . "'");
+		}
+
+		$this->cache->delete('product');
+	}
+	// Pez Globo End
+}
