8e31fee9dc176b9c0d362e38ce84a41d1b7d1616
Desarrollo <desarrollo@webifica.com>
2023-06-19 12:32:37 -0500

    agregar un nuevo stado del proceso

--- upload/catalog/controller/pezglobo/domain/PezGloboProcessStatus.php
+++ upload/catalog/controller/pezglobo/domain/PezGloboProcessStatus.php
@@ -1,6 +1,7 @@
 <?php
 final class PezGloboProcessStatus {
-    const IN_QUEUE = 1;
-    const IN_PROGRESS = 2;
-    const FINISHED = 3;
+    const AGGREGATOR_IN_QUEUE = 1;
+    const AGGREGATOR_IN_PROGRESS = 2;
+    const AGGREGATOR_FINISHED = 3;
+    const SYNCHRONIZER_FINISHED = 4;
 }
